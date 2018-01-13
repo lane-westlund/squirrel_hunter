5ea6707b4136c32f37759eea6867fb14f3e25f1a
Lane Westlund <thefirelane@gmail.com>
2018-01-13 15:14:58 +0100

    feat(ImageSetter): add json output file for cropping info, close #14

--- ImageSetter.py
+++ ImageSetter.py
@@ -1,6 +1,9 @@
 import os
 import argparse
 import cv2
+import json
+import io
+import os
 
 # initialize the list of reference points and boolean indicating
 # whether cropping is being performed or not
@@ -38,6 +41,13 @@ cv2.namedWindow("image")
 cv2.setMouseCallback("image", click_and_crop)
 
 output_dir = args.dir + '../output'
+json_path = args.dir + 'crops.json'
+
+if os.path.isfile(json_path) and os.access(json_path, os.R_OK):
+    crop_info = json.load(open(json_path))
+else:
+    io.open(os.path.join(args.dir, 'crops.json'), 'w')
+    crop_info = {}
 
 try:
     os.makedirs(output_dir)
@@ -46,22 +56,40 @@ except OSError:
 
 for current_file in os.listdir(args.dir):
     successful = False
+
+    if current_file in crop_info:
+        continue
+
     while not successful:
         current_image = cv2.imread( args.dir + current_file, cv2.IMREAD_COLOR)
         clone = current_image.copy()
         cv2.imshow("image", current_image)
         cv2.waitKey(0)
         if len(refPt) == 2:
+            top_x = refPt[0][1]
+            top_y = refPt[1][1]
+            bot_x = refPt[0][0]
+            bot_y = refPt[1][0]
             roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
             cv2.imshow("ROI", roi)
             key = cv2.waitKey(0)
+            file_data = {}
             if (key == ord('s') or key == ord('k')):
                 #s == save k == sKip
                 if(key == ord('s')):
                     cv2.imwrite(output_dir + "/" + current_file, roi)
-                os.remove(args.dir + current_file)
+                    file_data["top_x"] = top_x
+                    file_data["top_y"] = top_y
+                    file_data["bot_x"] = bot_x
+                    file_data["bot_y"] = bot_y
+                    file_data["skipped"] = False
+                if (key == ord('k')):
+                    file_data["skipped"] = True
                 successful = True
+                crop_info[current_file] = file_data
             if key == ord('q'):
+                with open(json_path, 'w') as outfile:
+                    json.dump(crop_info, outfile)
                 cv2.destroyAllWindows()
                 quit()
 
